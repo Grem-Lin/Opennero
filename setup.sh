d4bed6ef63cba0f7f045d217d65eb4bccac2398e
Grem-Lin <44858182+Grem-Lin@users.noreply.github.com>
2019-08-29 22:00:45 -0500

    add setup.sh file for install

---
+++ setup.sh
@@ -0,0 +1,21 @@
+apt-get install \
+    subversion \
+    cmake \
+    g++ \
+    doxygen \
+    libgl1-mesa-dev \
+    libx11-dev \
+    libxxf86vm-dev \
+    zlib1g-dev \
+    libbz2-dev \
+    libpng++-dev \
+    libjpeg-dev \
+    libboost-dev \
+    libboost-python-dev \
+    libboost-filesystem-dev \
+    libboost-serialization-dev \
+    libboost-system-dev \
+    libboost-date-time-dev \
+    maven \
+    openjdk-8-jdk
+
